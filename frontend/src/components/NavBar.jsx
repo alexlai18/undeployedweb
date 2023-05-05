34b0c4a0b1b24054fc6ad14c06888ed4bb2eb68f
Alexander <alexander.l.lai@student.unsw.edu.au>
2023-05-05 23:11:27 +1000

    fix: Logo links to frontpage instead of another route

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar () {
         <>
           <AppBar position="static" style={{ background: '#b28e59', height: 100, overflow: 'hidden' }}>
             <Toolbar disableGutters>
-              <Link to='/dashboard' style={{ display: 'flex', flexDirection: 'row', textDecoration: 'none', paddingLeft: 40, width: '40%' }}>
+              <Link to='/' style={{ display: 'flex', flexDirection: 'row', textDecoration: 'none', paddingLeft: 40, width: '40%' }}>
                 <img src={logo} alt='personal-logo' style={{ width: '40%' }}/>
                 &nbsp; &nbsp; &nbsp;
               </Link>
